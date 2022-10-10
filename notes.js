b07d8eb9dcc913578b5049029c43a43988aa9966
soufianeargane <soufianeargane800@gmail.com>
2022-10-10 18:31:44 +0100

    so much

--- notes.js
+++ notes.js
@@ -1,4 +1,4 @@
 // relative: to makr position relative
 // container: for container
 // m for margin, p for padding 
-// x for left and write and then L (not capital letters tho) for left...etc
+// (x for left and write) and then L (not capital letters tho) for left...etc
